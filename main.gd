02cc1411c83e86006981a8f6bff30d8b8936971f
adonispujols <adonispujols@users.noreply.github.com>
2018-11-05 10:31:25 -0500

    start of game and stack definitions
    still missing resolve next for stack

---
+++ main.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+#	var _game_class = load("game.gd")
+#	var game = _game_class.new(2)
+#	print(game.get_val())
+#	game.set_val(3)
+#	print(game.get_val())
+	pass
---
+++ stack.gd
@@ -0,0 +1,19 @@
+#extends "res://stack.gd"
+
+var stack
+
+func _init():
+	stack = []
+
+func push(object):
+	stack.push_back(object)
+
+func _pop():
+	return stack.pop_back()
+
+func is_empty():
+	return stack.empty()
+
+func resolve_next():
+	pass
+#	_pop().resolve()
