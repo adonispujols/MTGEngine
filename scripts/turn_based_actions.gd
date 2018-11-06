a1bd09ad18e365951cbd579ccea92054532c4429
adonispujols <adonispujols@users.noreply.github.com>
2018-11-05 23:57:10 -0500

    slightly more progress done on turn based actions

--- scripts/turn_based_actions.gd
+++ scripts/turn_based_actions.gd
@@ -1,21 +1,3 @@
-"""
-< List of useful constants >
-- list of phase indices:
-* UNTAP_STEP = 0
-* UPKEEP_STEP = 1
-* DRAW_STEP = 2
-* PRE_COMBAT_PHASE = 3
-* BEGIN_COMBAT_STEP = 4
-* DECLARE_ATTACKERS_STEP = 5
-* DECLARE_BLOCKERS_STEP = 6
-* FIRST_STRIKE_DAMAGE = 7
-* COMBAT_DAMAGE_STEP = 8
-* END_OF_COMBAT_STEP = 9
-* POST_COMBAT_PHASE = 10
-* END_ STEP = 11
-* CLEANUP_STEP = 12
-< /List of useful constants >
-"""
 # list of phase indices
 const UNTAP_STEP = 0
 const UPKEEP_STEP = 1
@@ -31,18 +13,38 @@ const POST_COMBAT_PHASE = 10
 const END_STEP = 11
 const CLEANUP_STEP = 12
 
-# list holding methods
-start_step_or_phase_methods = []
+# list holding method names as strings
+const start_step_or_phase_methods = []
 
 # methods
-func start_next_step_or_phase(game):
-	steps_and_pahses_methods[game.get_current_step_or_phase()]
+static func start_next_step_or_phase(game):
+	call(start_step_or_phase_methods[game.get_current_step_or_phase()], game)
 
+static func untap_step_start(game):
+	game.set_step_or_phase(0)
+	game.change_active_player_to_next_player()
+	# untap all permanents()
+	
+	
+static func 
+static func 
+static func 
+static func 
+static func 
+static func 
+static func 
+static func 
+static func 
+static func 
+static func 
 """
-* start_next_step_or_phase(game object to work with)
-^- Called when a phase/step ends.
-- Keep updating until it's all implemented COMPLETELY
-^- As [CR 703.1] states, these do NOT use the stack.
-- simply calls steps_and_phases_methods[game.get_current_phase](game)
-- we COULD create a game variable associated with this script so we DON'T need to pass the game param everywhere (but this seems rather clunky AND pot. buggy so no).
+* untap_step_start(game)
+- game.set_step_or_phase(0)
+^- this COULD be made more efficient/automated on end_phase... call BUT risks making code buggy (so we won't).
+- game.change_active_player_to_next_player():
+^- set the active player to the next player
+- untap all permanents (if able/barring costs)
+^- in ruling: to "determine what to untap" means to check if there are any effect preventing something from untapping.
+- skip immediately to untap step because NO ONE receives priority during this step [CR 502.3]:
+- "No player receives priority....Triggers during this step are held until next time a player would receive priority, which is usually during the upkeep step. (See rule 503, “Upkeep Step.”)"
 """
